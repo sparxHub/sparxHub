5d2d0fa38c7e906ccc071d986a510466c44d91a1
Nadav Daniel <daniel.nadav@gmail.com>
2016-02-20 17:23:42 -0600

    some fixes for review

--- new/js/webflow.js
+++ new/js/webflow.js
@@ -57,15 +57,17 @@
 	__webpack_require__(7);
 	__webpack_require__(8);
 	__webpack_require__(9);
-	__webpack_require__(1);
 	__webpack_require__(10);
 	__webpack_require__(11);
+	__webpack_require__(1);
 	__webpack_require__(12);
 	__webpack_require__(13);
 	__webpack_require__(14);
 	__webpack_require__(15);
 	__webpack_require__(16);
-	module.exports = __webpack_require__(17);
+	__webpack_require__(17);
+	__webpack_require__(18);
+	module.exports = __webpack_require__(19);
 
 
 /***/ },
@@ -86,7 +88,7 @@
 	var $win = $(window);
 	var $doc = $(document);
 	var isFunction = $.isFunction;
-	var _ = Webflow._ = __webpack_require__(18);
+	var _ = Webflow._ = __webpack_require__(20);
 	var tram = __webpack_require__(3) && $.tram;
 	var domready = false;
 	var destroyed = false;
@@ -459,6 +461,65 @@
 	'use strict';
 
 	/**
+	 * Webflow: Background Video component
+	 */
+
+	var Webflow = __webpack_require__(1);
+
+	Webflow.define('backgroundVideo', module.exports = function ($, _) {
+	  var namespace = '.w-background-video';
+	  var doc = $(document);
+
+	  function ready () {
+	    var backgroundVideoNodes = $(document).find('.w-background-video');
+	    if (backgroundVideoNodes.length === 0) {
+	      return;
+	    }
+
+	    backgroundVideoNodes.each(function (_, node) {
+	      $(node).prepend(createVideoNode(node));
+	    });
+	  }
+
+	  function createVideoNode (nativeNode) {
+	    var nodeData = nativeNode.dataset;
+
+	    // Prevent loading the videos on mobile browsers as its likely that they
+	    // are on low-bandwidth connections.
+	    if (Webflow.isMobile()) {
+	      return $('<video />').css('background-image', 'url(' + nodeData.posterUrl + ')');
+	    }
+
+	    var videoURLs = nodeData.videoUrls.split(',');
+	    var sourceNodes = videoURLs.map(function (url) {
+	      return $('<source />').attr({
+	        src: url,
+	        'data-wf-ignore': '',
+	      });
+	    });
+
+	    var videoNode = $('<video />').attr({
+	      autoplay: nodeData.autoplay,
+	      loop: nodeData.loop,
+	    })
+	    .css('background-image', 'url(' + nodeData.posterUrl + ')');
+
+	    videoNode.append(sourceNodes);
+
+	    return videoNode;
+	  }
+
+	  return { ready: ready };
+	});
+
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	/**
 	 * Webflow: Brand pages on the subdomain
 	 */
 
@@ -533,7 +594,7 @@
 
 
 /***/ },
-/* 5 */
+/* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -733,7 +794,7 @@
 
 
 /***/ },
-/* 6 */
+/* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -839,7 +900,26 @@
 
 
 /***/ },
-/* 7 */
+/* 8 */
+/***/ function(module, exports) {
+
+	/**
+	 * Returns a Boolean representing whether or not the client is a mobile browser.
+	 *
+	 * NOTE: Many thanks to detectmobilebrowsers.com for this user agent detection
+	 * regex, without which the mobile internet probably wouldn't exist.
+	 */
+	Webflow.isMobile = function () {
+	  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+	  return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i
+	         .test(userAgent) ||
+	         /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i
+	         .test(userAgent.substr(0, 4));
+	}
+
+
+/***/ },
+/* 9 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/**
@@ -852,7 +932,7 @@
 	  var api = {};
 
 	  // Cross-Domain AJAX for IE8
-	  __webpack_require__(19);
+	  __webpack_require__(21);
 
 	  var $doc = $(document);
 	  var $forms;
@@ -1134,7 +1214,7 @@
 
 
 /***/ },
-/* 8 */
+/* 10 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -1170,7 +1250,7 @@
 
 
 /***/ },
-/* 9 */
+/* 11 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -1602,7 +1682,7 @@
 
 
 /***/ },
-/* 10 */
+/* 12 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -2269,7 +2349,7 @@
 
 
 /***/ },
-/* 11 */
+/* 13 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -2381,7 +2461,7 @@
 
 
 /***/ },
-/* 12 */
+/* 14 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -2605,7 +2685,7 @@
 
 
 /***/ },
-/* 13 */
+/* 15 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -2951,7 +3031,7 @@
 
 
 /***/ },
-/* 14 */
+/* 16 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -3109,7 +3189,7 @@
 
 
 /***/ },
-/* 15 */
+/* 17 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -3619,7 +3699,7 @@
 
 
 /***/ },
-/* 16 */
+/* 18 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -3836,7 +3916,7 @@
 
 
 /***/ },
-/* 17 */
+/* 19 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -3978,7 +4058,7 @@
 
 
 /***/ },
-/* 18 */
+/* 20 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -4338,7 +4418,7 @@
 
 
 /***/ },
-/* 19 */
+/* 21 */
 /***/ function(module, exports) {
 
 	/*!
