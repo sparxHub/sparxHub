88a8a7844fbfc3fec467124b93f5b5fef4bad9ae
Nadav Daniel <daniel.nadav@gmail.com>
2016-05-17 11:36:00 -0500

    Adding param to create app buttons

--- js/webflow.js
+++ js/webflow.js
@@ -467,9 +467,6 @@
 	var queueIndex = -1;
 
 	function cleanUpNextTick() {
-	    if (!draining || !currentQueue) {
-	        return;
-	    }
 	    draining = false;
 	    if (currentQueue.length) {
 	        queue = currentQueue.concat(queue);
