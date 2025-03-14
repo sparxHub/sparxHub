7d20cad975e27bf8eb48ba493999341f81ae01ac
Nadav Daniel <nadav@appv.io>
2025-03-14 17:11:03 +0200

    feat: Integrate Astro blog starter kit
    - Configure blog structure with content collections support
    - Add Markdown & MDX support for content
    - Configure Prettier to ignore MDX files

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,29 +1,49 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
+// import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
+// import AutoImport from 'astro-auto-import';
+import astroExpressiveCode from 'astro-expressive-code'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.nadav-daniel.com', // Set the correct blog site domain
-  // base: '/blog/',  // Ensures all paths start with /blog/
+  base: '/blog/',  // Ensures all paths start with /blog/
   outDir: '../out/blog/', // Ensure Astro outputs to the correct folder
   output: 'static', // Generate static site
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [
+    //FIXME: AutoImport to avoid imports on content files
+    // AutoImport({
+    //   imports: ['./src/components/BlockCode.astro', './src/components/BlockBold.astro']
+    // }),
+    // sitemap(),
+
+    astroExpressiveCode({
+      // You can set configuration options here
+      themes: ['dracula', 'aurora-x'],
+      styleOverrides: {
+        // You can also override styles
+        borderRadius: '0.5rem',
+        frames: {
+          shadowColor: '#124',
+        },
+      },
+    }),
+    mdx(), react()],
   assetsInclude: ['**/*.woff2', '**/*.woff', '**/*.ttf'],
   build: {
     // assets: '/blog/',  // Ensures assets like images and scripts are resolved correctly
   },
 
-  // vite: {
-  //   server: {
-  //     proxy: {
-  //       '/blog/': {
-  //         target: 'http://localhost:4322',  // Ensure correct dev proxy
-  //         changeOrigin: true,
-  //         rewrite: (path) => path.replace(/^\/blog/, ''),
-  //       },
-  //     },
-  //   },
-  // },
+  vite: {
+    server: {
+      proxy: {
+        '/blog/': {
+          target: 'http://localhost:4322',  // Ensure correct dev proxy
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/blog/, ''),
+        },
+      },
+    },
+  },
 })
