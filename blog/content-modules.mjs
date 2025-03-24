18bf33f8cd0187d5912ad3a605434e06d205c649
Nadav Daniel <nadav@appv.io>
2025-03-24 17:36:03 +0200

    Updates

--- blog/content-modules.mjs
+++ blog/content-modules.mjs
@@ -1,5 +1,5 @@
 
 export default new Map([
-["src/content/blog/monolithic-architecture-mvps.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmonolithic-architecture-mvps.mdx&astroContentModuleFlag=true")],
-["src/content/blog/hexagonal-architecture-frontend.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhexagonal-architecture-frontend.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/hexagonal-architecture-frontend.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhexagonal-architecture-frontend.mdx&astroContentModuleFlag=true")],
+["src/content/blog/monolithic-architecture-mvps.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmonolithic-architecture-mvps.mdx&astroContentModuleFlag=true")]]);
 		
